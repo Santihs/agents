94d3e0945d8c2e15ffaa181abf02f9662918bf05
Santiago Huanacu Sanchez <santihs.sanchez.dev@gmail.com>
2025-12-19 22:59:04 -0400

    Complete labs using my own KEYS, modify and understand code examples

--- 1_foundations/app.py
+++ 1_foundations/app.py
@@ -76,7 +76,8 @@ tools = [{"type": "function", "function": record_user_details_json},
 class Me:
 
     def __init__(self):
-        self.openai = OpenAI()
+        self.groq_api_key = os.getenv('GROQ_API_KEY')
+        self.openai = OpenAI(api_key=self.groq_api_key, base_url="https://api.groq.com/openai/v1")
         self.name = "Ed Donner"
         reader = PdfReader("me/linkedin.pdf")
         self.linkedin = ""
@@ -116,7 +117,7 @@ If the user is engaging in discussion, try to steer them towards getting in touc
         messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
         done = False
         while not done:
-            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            response = self.openai.chat.completions.create(model="openai/gpt-oss-120b", messages=messages, tools=tools)
             if response.choices[0].finish_reason=="tool_calls":
                 message = response.choices[0].message
                 tool_calls = message.tool_calls
